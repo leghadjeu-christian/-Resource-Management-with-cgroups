01d63a794e0361f8d7594fa7dafdb401a236d97b
Leghadjeu Christian <legochristianarthur@gmail.com>
2025-01-14 12:38:40 +0100

    piping  the "grep  cgroups "to the result of docker stats

--- script2.sh
+++ script2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 count=1
   while [ $count -ne 7 ]; do
-        docker stats --no-stream  | cat  >> stats.log 
+        docker stats --no-stream  | grep cgroups | cat  >> stats.log 
 ((count ++))   
     sleep 1
     done
